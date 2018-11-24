7f27e523ac63b84f910b3ef13e01340d5300546a
Suvajit Sahoo <31549314+isuvajitsahoo@users.noreply.github.com>
2018-11-24 18:01:14 +0530

    Add files via upload

---
+++ jQuery_AJAX_WebAPI_CRUD_MVC/Controllers/AjaxAPIController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Web.Http;
+using System.Linq;
+
+namespace jQuery_AJAX_WebAPI_CRUD_MVC.Controllers
+{
+    public class AjaxAPIController : ApiController
+    {
+        [Route("api/AjaxAPI/InsertCustomer")]
+        [HttpPost]
+        public Customer1234 InsertCustomer(Customer1234 _customer)
+        {
+            using (Training_19Sep18_PuneEntities1 entities = new Training_19Sep18_PuneEntities1())
+            {
+                entities.Customer1234.Add(_customer);
+                entities.SaveChanges();
+            }
+
+            return _customer;
+        }
+
+        [Route("api/AjaxAPI/UpdateCustomer")]
+        [HttpPost]
+        public bool UpdateCustomer(Customer1234 _customer)
+        {
+            using (Training_19Sep18_PuneEntities1 entities = new Training_19Sep18_PuneEntities1())
+            {
+                Customer1234 updatedCustomer = (from c in entities.Customer1234
+                                            where c.CustomerId == _customer.CustomerId
+                                            select c).FirstOrDefault();
+                updatedCustomer.Name = _customer.Name;
+                updatedCustomer.Country = _customer.Country;
+                entities.SaveChanges();
+            }
+
+            return true;
+        }
+
+        [Route("api/AjaxAPI/DeleteCustomer")]
+        [HttpPost]
+        public void DeleteCustomer(Customer1234 _customer)
+        {
+            using (Training_19Sep18_PuneEntities1 entities = new Training_19Sep18_PuneEntities1())
+            {
+                Customer1234 customer = (from c in entities.Customer1234
+                                     where c.CustomerId == _customer.CustomerId
+                                     select c).FirstOrDefault();
+                entities.Customer1234.Remove(customer);
+                entities.SaveChanges();
+            }
+        }
+    }
+}
---
+++ jQuery_AJAX_WebAPI_CRUD_MVC/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jQuery_AJAX_WebAPI_CRUD_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Training_19Sep18_PuneEntities1 entities = new Training_19Sep18_PuneEntities1();
+            List<Customer1234> customers = entities.Customer1234.ToList();
+            if (customers.Count == 0)
+            {
+                customers.Add(new Customer1234());
+            }
+
+            return View(customers);
+        }
+    }
+}
